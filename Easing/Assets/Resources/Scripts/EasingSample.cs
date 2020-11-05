4a65b692bf1fbdae270db2e25d2b1c204fd9c4b1
a_ota <kd1268987@st.kobedenshi.ac.jp>
2020-11-05 12:51:15 +0900

    Easing

---
+++ Easing/Assets/Resources/Scripts/EasingSample.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EasingSample : MonoBehaviour
+{
+    public bool isDrawBox = true;
+    public bool isDrawGridL = false;
+    public bool isDrawGridM = false;
+    public bool isDrawLoad = false;
+    [SerializeField] EasingTest easingTest;
+
+
+    private void OnDrawGizmos()
+    {
+        if (isDrawBox)
+        {
+            Gizmos.DrawLine(transform.position + new Vector3(1, 1, 0), transform.position + new Vector3(1, 0, 0));
+            Gizmos.DrawLine(transform.position + new Vector3(1, 0, 0), transform.position + new Vector3(0, 0, 0));
+            Gizmos.DrawLine(transform.position + new Vector3(0, 0, 0), transform.position + new Vector3(0, 1, 0));
+            Gizmos.DrawLine(transform.position + new Vector3(0, 1, 0), transform.position + new Vector3(1, 1, 0));
+        }
+        if (isDrawGridL)
+        {
+            Gizmos.DrawLine(transform.position + new Vector3(0, 0.5f, 0), transform.position + new Vector3(1, 0.5f, 0));
+            Gizmos.DrawLine(transform.position + new Vector3(0.5f, 0, 0), transform.position + new Vector3(0.5f, 1, 0));
+        }
+        if (isDrawGridM)
+        {
+            Gizmos.DrawLine(transform.position + new Vector3(0, 0.25f, 0), transform.position + new Vector3(1, 0.25f, 0));
+            Gizmos.DrawLine(transform.position + new Vector3(0.25f, 0, 0), transform.position + new Vector3(0.25f, 1, 0));
+            Gizmos.DrawLine(transform.position + new Vector3(0, 0.75f, 0), transform.position + new Vector3(1, 0.75f, 0));
+            Gizmos.DrawLine(transform.position + new Vector3(0.75f, 0, 0), transform.position + new Vector3(0.75f, 1, 0));
+        }
+        if (isDrawLoad)
+        {
+            for (float x = 0; x <= 1; x += 0.01F)
+            {
+                float y = Easing.GetEasing(x, easingTest.type);
+                Gizmos.DrawSphere(transform.position + new Vector3(x, y, 0), 0.01f);
+            }
+        }
+    }
+}
---
+++ Easing/Assets/Resources/Scripts/EasingTest.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EasingTest : MonoBehaviour
+{
+    [SerializeField, Header("目標時間(イージングの変化を確認する時間)")]
+    float targetTime = 3.0f;
+
+    [Header("イージングの種類")]
+    public EasingTypes type;
+
+    [Header("イージングの影響を受けるゲームオブジェクト=================")]
+    [SerializeField] Slider     slider;
+    [SerializeField] GameObject cube;
+    [SerializeField] GameObject sphere;
+    [SerializeField] GameObject plane;
+    [SerializeField] GameObject image;
+
+    float deltaTime = 0.0f;     //経過時間
+    float targetImageX = 1000.0f;
+
+    void Start()
+    {
+    }
+   
+    void Update()
+    {
+        //前回のフレームから経過した時間を追加
+        deltaTime += Time.deltaTime;
+        deltaTime = Mathf.Clamp(deltaTime,0.0f, targetTime);
+
+        float value = Easing.GetEasing(deltaTime/ targetTime, type);
+        
+        //スライダーの値にイージングの結果を代入する
+        slider.value = value;
+
+        cube.transform.localScale = new Vector3(value, value, value);
+        sphere.transform.localScale = new Vector3(value, value, value);
+
+        float x = value * targetImageX;
+        var position = image.transform.position;
+        position.x = x;
+        image.transform.position = position;
+    }
+
+    public void Reset()
+    {
+        deltaTime = 0.0f;
+    }
+}
